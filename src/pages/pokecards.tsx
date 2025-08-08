194e5dcd3eceb0a39576dfa23f69f03f9f55273b
Anudeep <anudeep.roy@liferay.com>
2025-08-08 16:54:00 +0530

    Trying to fetch TCG api

---
+++ src/pages/pokecards.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react"
+import Header from "./sections/header"
+
+export default function Pokecards() {
+
+    const [pokemon, setPokemon] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [pokeId, setPokeId] = useState('');
+    const endpoint = `https://api.pokemontcg.io/v2/cards`;
+
+    const myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    myHeaders.append('X-Api-Key', '1a104ce1-a5da-4cee-ad55-c848b0ac0235');
+
+    const requestOptions:any = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+    };
+
+    useEffect(() => {
+        fetch(`${endpoint}/base1-4`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                setPokemon(data.data.name);
+                setLoading(false);
+            })
+             .catch((error) => {
+                console.error("Error fetching card:", error);
+                setLoading(false); // ✅ Also handle loading on error
+            });
+    }, [])
+
+    return (
+        <div className="pokecards">
+            <Header />
+            {loading ? (
+                <p>Loading</p>
+            ): (
+                pokemon && <p>{pokemon}</p>
+            )}
+        </div>
+    )
+}
